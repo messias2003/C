e3194bf11073318324921fbec3e0e48236c9a841
messias2003 <messiassantiago1020@gmail.com>
2026-04-01 21:18:30 -0300

    Add files via upload

---
+++ Número par.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main () {
+
+
+
+    int  n;
+
+    printf("Digite qualquer numero: ");
+    scanf ("%d", &n);
+    while (n <=10) {
+    if (n%2==0){
+            printf (" Este numero e par %d\n",n);
+        }else{
+            printf ("\n");
+}
+n++;
+
+}
+
+
+
+
+
+
+return 0;
+
+}
